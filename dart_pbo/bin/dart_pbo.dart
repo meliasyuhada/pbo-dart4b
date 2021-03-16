e5b637a8a6051ec75130fa459efef33831360ca8
Melia Syuhada <meliasyuhada5@gmail.com>
2021-03-17 04:08:38 +0700

    edit list mapping

--- dart_pbo/bin/dart_pbo.dart
+++ dart_pbo/bin/dart_pbo.dart
@@ -4,7 +4,7 @@ void main(List<String> arguments) {
   List<int> myList = [];
   List<int> list = [1,2,3];
 
-  for (int index = 0; index < list.length; index += 1){
-    
+  for (int bilangan in list){
+    print(bilangan);
   }
 }
