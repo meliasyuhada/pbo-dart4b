c1bf0b2a837028ee0314c4b98c4bbcfd4f949ad2
Melia Syuhada <meliasyuhada5@gmail.com>
2021-03-17 02:35:57 +0700

    tambahkan

---
+++ dart_pbo/bin/dart_pbo.dart
@@ -0,0 +1,6 @@
+import 'package:dart_pbo/dart_pbo.dart' as dart_pbo;
+import 'dart:io';
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_pbo.calculate()}!');
+}
---
+++ dart_pbo/test/dart_pbo_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_pbo/dart_pbo.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
