e4f6cd37c549efaddf120b1aa7574f206427bafd
Melia Syuhada <meliasyuhada5@gmail.com>
2021-03-17 20:54:04 +0700

    rubah

--- dart_pbo/bin/list_mapping.dart
+++ dart_pbo/bin/list_mapping.dart
@@ -1,10 +1,15 @@
 import 'dart:io';
 
-main(List<String> arguments) {
-  List<int> myList = [1, 2, 3, 4, 5, 6, 7];
-  List<int> list = [1, 2, 3]; 
+void main(){
+  // membuat list dengan ukuran 5
+  var pelajaran = new List(5);
 
-  myList.removeWhere((number) => number % 2 )
+  print("Sebutkan ${pelajaran.length} yang ingin kamu pelajar!");
 
- 
+  for(int i = 0; i < pelajaran.length; i++){
+      stdout.write("$i. ");
+      pelajaran[i] = stdin.readLineSync();
+  }
+
+  print(pelajaran);
 }
