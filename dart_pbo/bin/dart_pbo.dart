2a707a7e2c7686f716fa64001c24728dfa857373
Melia Syuhada <meliasyuhada5@gmail.com>
2021-03-17 18:46:02 +0700

    edit list1

--- dart_pbo/bin/dart_pbo.dart
+++ dart_pbo/bin/dart_pbo.dart
@@ -1,12 +1,18 @@
 import 'dart:io';
 
-void main(List<String> arguments) {
+main(List<String> arguments) {
   List<int> myList = [];
-  List<int> list = [1,2,3];
+  List<int> list = [1,2,3]; 
 
   // for (int bilangan in list){
   //   print(bilangan);
   // }
+
+  myList.add(10);
+  myList.addAll(list);
+  myList.insert(1, 20);
+  myList.insertAll(3, [30, 40]);
+  myList.remove(20);
 
   list.forEach((bilangan) {
     print(bilangan);
