c62f40ccbe08eae13252b6daa78a04c8f9916c05
Melia Syuhada <meliasyuhada5@gmail.com>
2021-03-17 18:52:47 +0700

    lanjutan

---
+++ dart_pbo/bin/list_mapping.dart
@@ -0,0 +1,10 @@
+import 'dart:io';
+
+main(List<String> arguments) {
+  List<int> myList = [1, 2, 3, 4, 5, 6, 7];
+  List<int> list = [1, 2, 3]; 
+
+  myList.removeWhere((number) => number % 2 )
+
+ 
+}
