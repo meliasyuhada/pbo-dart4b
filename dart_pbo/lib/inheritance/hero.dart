4425095bbb9f39410927bb12e48017dfa900110d
Melia Syuhada <meliasyuhada5@gmail.com>
2021-03-21 20:48:55 +0700

    edit edit

---
+++ dart_pbo/lib/inheritance/hero.dart
@@ -0,0 +1,5 @@
+import 'package:dart_pbo/inheritance/character.dart';
+
+class Hero extends Character {
+  String killAMonster() => "Take this";
+}
--- dart_pbo/lib/inheritance/main.dart
+++ dart_pbo/lib/inheritance/main.dart
@@ -1,2 +1,17 @@
 import 'dart:io';
 
+import 'package:dart_pbo/inheritance/hero.dart';
+import 'package:dart_pbo/inheritance/monster.dart';
+
+main(List<String> args) {
+  Hero h = Hero();
+  Monster m = Monster();
+
+  h.healthPoint = -10;
+  m.healthPoint = 10;
+
+  print("hero HP: " + h.healthPoint.toString());
+  print("monster HP: " + m.healthPoint.toString());
+  print(h.killAMonster());
+  print(m.eatHuman());
+}
