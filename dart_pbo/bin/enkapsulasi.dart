ba17b03fe97762c0e28659ceba4db0bdc72c5698
Melia Syuhada <meliasyuhada5@gmail.com>
2021-03-21 19:16:25 +0700

    enkapsulasi

---
+++ dart_pbo/bin/enkapsulasi.dart
@@ -0,0 +1,14 @@
+class PersegiPanjang {
+  double _panjang;
+  double lebar;
+
+  void setPanjang(double value) {
+    if(value < 0) {
+      value *= -1;
+    }
+    _panjang = value;
+  }
+  double hitungLuas() {
+    return this._panjang * lebar;
+  }
+}
