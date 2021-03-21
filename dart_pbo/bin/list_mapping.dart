ac9cdb1b8d40b2eb2962977cee7e73bf0dc81ac4
Melia Syuhada <meliasyuhada5@gmail.com>
2021-03-21 09:41:16 +0700

    lis mapping

--- dart_pbo/bin/list_mapping.dart
+++ dart_pbo/bin/list_mapping.dart
@@ -2,14 +2,28 @@ import 'dart:io';
 
 void main(){
   // membuat list dengan ukuran 5
-  var pelajaran = new List(5);
+//   var pelajaran = new List(5);
 
-  print("Sebutkan ${pelajaran.length} yang ingin kamu pelajar!");
+//   print("Sebutkan ${pelajaran.length} yang ingin kamu pelajar!");
 
-  for(int i = 0; i < pelajaran.length; i++){
-      stdout.write("$i. ");
-      pelajaran[i] = stdin.readLineSync();
-  }
+//   for(int i = 0; i < pelajaran.length; i++){
+//       stdout.write("$i. ");
+//       pelajaran[i] = stdin.readLineSync();
+//   }
 
-  print(pelajaran);
+//   print(pelajaran);
+double luas_segiempat(double panjang, double lebar) {
+  double hasil;
+  hasil = panjang * lebar;
+  return hasil;
 }
+
+main(List()) {
+  double p, l, luas;
+
+  p = double.tryParse(stdin.readLineSync());
+  l = double.tryParse(stdin.readLineSync());
+
+  luas = luas_segiempat(p, l);
+}
+ }
