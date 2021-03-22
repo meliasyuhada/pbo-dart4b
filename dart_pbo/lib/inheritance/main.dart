10915e9dbc39a092242a89221e4bbcd1e7ea4adc
Melia Syuhada <meliasyuhada5@gmail.com>
2021-03-22 08:08:34 +0700

    main edit

--- dart_pbo/lib/inheritance/main.dart
+++ dart_pbo/lib/inheritance/main.dart
@@ -1,24 +1,28 @@
 import 'dart:io';
 
+import 'package:dart_pbo/inheritance/flying_monster.dart';
 import 'package:dart_pbo/inheritance/hero.dart';
 import 'package:dart_pbo/inheritance/monster.dart';
 import 'package:dart_pbo/inheritance/monster_kecoa.dart';
 import 'package:dart_pbo/inheritance/monster_ubur-ubur.dart';
+import 'package:dart_pbo/inheritance/monster_ucoak.dart';
 
-main(List<String> args) {
-  Hero h = Hero();
-  Monster m = Monster();
-  MonsterUburUbur u = MonsterUburUbur();
+void main(List<String> arguments) async {
+  // Hero h = Hero();
+  // Monster m = Monster();
+  // MonsterUburUbur u = MonsterUburUbur();
   List<Monster> monsters =[];
 
   monsters.add(MonsterUburUbur());
   monsters.add(Monsterkecoa());
-  monsters.add(MonsterUburUbur());
+  monsters.add(MonsterUcoa());
 
 
 
   for (Monster m in monsters) {
-    print(m.move());
+    if (m is FlyingMonster) {
+      print((m as FlyingMonster).fly());
+    }
   }
 
   // h.healthPoint = -10;
