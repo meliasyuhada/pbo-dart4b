1a9893e070765ded502b0c6ce8227ad740bff748
Melia Syuhada <meliasyuhada5@gmail.com>
2021-03-22 08:09:16 +0700

    bur ubur mon

--- dart_pbo/lib/inheritance/monster_ubur-ubur.dart
+++ dart_pbo/lib/inheritance/monster_ubur-ubur.dart
@@ -3,6 +3,12 @@ import 'package:dart_pbo/inheritance/monster.dart';
 class MonsterUburUbur extends Monster{
   String swim() => "Woosh ... woosh..";
 
+  @override
+  String eatHuman() {
+    // TODO: implement eatHuman
+    return "Sedot-sedot asik";
+  }
+
   @override 
   String move() {
     return "Berenang-renang";
