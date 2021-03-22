02efe2d0e894d08fb39aff3541343d8089edd848
Melia Syuhada <meliasyuhada5@gmail.com>
2021-03-22 08:08:55 +0700

    kecoa mon

--- dart_pbo/lib/inheritance/monster_kecoa.dart
+++ dart_pbo/lib/inheritance/monster_kecoa.dart
@@ -1,6 +1,8 @@
+import 'package:dart_pbo/inheritance/flying_monster.dart';
 import 'package:dart_pbo/inheritance/monster.dart';
 
-class Monsterkecoa extends Monster {
+class Monsterkecoa extends Monster  implements FlyingMonster {
+  @override
   String fly() => "Syunggg...";
 
   @override 
